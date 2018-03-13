802e666007192888c1db8eef5ef36f1c254c39fd
Kion <kionkshamsa@gmail.com>
2018-03-12 20:48:31 -0600

    Gotta Catch em all

--- wwwroot/script.js
+++ wwwroot/script.js
@@ -1,7 +1,7 @@
 function DisplayLog(message) {
-    document.querySelector("#statusMessages").textContent += message + "<br/>";
+    document.querySelector("#statusMessages").textContent += message + "\r\n";
 }
 
 function DisplayCount(count) {
-    document.querySelector("#statusMessages").textContent += "Clients Connected: " + count + "<br/>"; 
+    document.querySelector("#statusMessages").textContent += "Clients Connected: " + count + "\r\n"; 
 }
