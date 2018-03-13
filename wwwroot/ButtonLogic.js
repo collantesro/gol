5ba6df9519388dbbe2f013b77cf46ef3fcf3aa8f
Kion <kionkshamsa@gmail.com>
2018-03-12 20:10:37 -0600

    BoardUpdate method and write to serverConnection

--- wwwroot/ButtonLogic.js
+++ wwwroot/ButtonLogic.js
@@ -3,7 +3,7 @@ function ButtonEventFire(ButtonEvent) {
     var row = b.getAttribute("data-row"); 
     var col = b.getAttribute("data-col");
     console.log("Row: " + row + " Column: " + col);
-
+    serverConnection.send("toggle:" + row + ":" + col);
     ButtonLife(b);
 }
 
@@ -12,4 +12,28 @@ function ButtonLife(b) {
     //var b = ButtonEvent.target;
     
     b.classList.toggle("alive");
+}
+
+//GBTag
+
+function BoardUpdate(flatString) {
+    var bttns = document.querySelector("#GBTag"); //document.body.childNodes
+    var b; 
+
+    var index = 0 
+
+    for(b in bttns) {
+        if(b.tagName == "Button") {
+            if(flatString.charAt(index) == "X") {
+                b.classList.add("alive"); 
+            }
+            else {
+                b.classList.remove("alive"); 
+            }
+            index++; 
+        }
+        else {
+            continue; 
+        }
+    }
 }
