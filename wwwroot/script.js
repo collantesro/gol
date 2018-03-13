f3d3e8f65933703cff78fa681335e1707b1a4a89
Kion <kionkshamsa@gmail.com>
2018-03-12 20:26:51 -0600

    I love horses

---
+++ wwwroot/script.js
@@ -0,0 +1,7 @@
+function DisplayLog(message) {
+    document.querySelector("#statusMessage").textContent += message + "<br/>";
+}
+
+function DisplayCount(count) {
+    document.querySelector("#statusMessage").textContent += count + "<br/>"; 
+}
